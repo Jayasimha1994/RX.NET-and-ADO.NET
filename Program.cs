deae8a7e6ff2d63a3fbc785e7fa0a25c4552790c
jayasimha <jayasimhajay1994@gmail.com>
2018-03-19 19:59:36 +0530

    RX.NET and ADO.NET programs

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace RXproject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write("enter empid: ");
+                int empid = Convert.ToInt32(Console.ReadLine());
+
+                Console.Write("enter the name :");
+                string name = Console.ReadLine();
+
+                string query = "insert into emp values(" +
+                    empid + ", '" + name + "' )";
+                Console.WriteLine(query);
+                string cs = @"Data Source=.\JAYASIMHASQL;Initial Catalog=con1;User ID=sa;Password=123";
+                SqlConnection con = new SqlConnection(cs);
+                con.Open();
+                Console.WriteLine("established connection ");
+                SqlCommand cmd = new SqlCommand(query, con);
+                int result = cmd.ExecuteNonQuery();
+                Console.WriteLine(result + " records inserted in emp table");
+
+                con.Close();
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e);
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ SubscriberDemo1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive;
+using System.Reactive.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RXproject
+{
+    class SubscriberDemo1
+    {
+        static void Main(string[] args)
+        {
+            IObservable<int> source = Observable.Range(1, 10);
+            IObserver<int> obsvr = Observer.Create<int>(
+                x => Console.WriteLine("OnNext: {0}", x),
+                ex => Console.WriteLine("OnError: {0}", ex.Message),
+                () => Console.WriteLine("OnCompleted"));
+            IDisposable subscription = source.Subscribe(obsvr);
+            Console.WriteLine("Press ENTER to unsubscribe...");
+            Console.ReadLine();
+            subscription.Dispose();
+        }
+    }
+}
